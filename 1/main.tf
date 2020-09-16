8adc301f5323ebde44d091c193000417ccce6a24
Radek Maziarka <maziarka.radoslaw@outlook.com>
2020-09-16 14:28:45 +0200

    Rename folder names

--- 1/main.tf
+++ 1/main.tf
@@ -3,3 +3,41 @@ provider "azurerm" {
   version = "=2.6.0"
   features {}
 }
+
+resource "azurerm_resource_group" "default" {
+  name     = "terraform-azure"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "default" {
+  name                     = "terraformazuretest"
+  resource_group_name      = azurerm_resource_group.default.name
+  location                 = azurerm_resource_group.default.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "default" {
+  name                  = "terraform"
+  storage_account_name  = azurerm_storage_account.default.name
+  container_access_type = "private"
+}
+
+
+
+
+
+
+
+
+
+
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "terraform-azure"
+    storage_account_name = "terraformazuretest"
+    container_name       = "terraform"
+    key                  = "terraform.tfstate"
+  }
+}
--- 2/main.tf
+++ 2/main.tf
@@ -1,46 +1,13 @@
-provider "azurerm" {
-  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
-  version = "=2.6.0"
-  features {}
-}
-
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "terraform-azure"
-    storage_account_name = "terraformazuretest"
-    container_name       = "terraform"
-    key                  = "terraform.tfstate"
-  }
-}
-
 resource "azurerm_resource_group" "default" {
-  name     = "terraform-azure"
+  name     = local.resource_group_name
   location = "West Europe"
 }
 
 resource "azurerm_storage_account" "default" {
-  name                     = "terraformazuretest"
-  resource_group_name      = azurerm_resource_group.default.name
-  location                 = azurerm_resource_group.default.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+  name                      = local.storage_account_name
+  resource_group_name       = azurerm_resource_group.default.name
+  location                  = azurerm_resource_group.default.location
+  account_tier              = "Standard"
+  account_replication_type  = "LRS"
+  account_kind              = "StorageV2"
 }
-
-resource "azurerm_storage_container" "default" {
-  name                  = "terraform"
-  storage_account_name  = azurerm_storage_account.default.name
-  container_access_type = "private"
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
