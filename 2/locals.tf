9b2291c0470c57f3ded7bd37197ebc61fb0b2064
Radek Maziarka <maziarka.radoslaw@outlook.com>
2020-09-16 15:09:09 +0200

    Change storage account name

--- 2/locals.tf
+++ 2/locals.tf
@@ -1,5 +1,5 @@
 locals {
-    resource_group_name  = "terraform-example"
+    resource_group_name  = "terraform-azure-2"
     first_network_name   = "${local.resource_group_name}-first"
     second_network_name  = "${local.resource_group_name}-second"
     storage_account_name = "${replace(local.resource_group_name, "-", "")}sa"
